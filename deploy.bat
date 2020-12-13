03fadc67bcba0518d49086bb974bb9dc7b1b4981
Nicholas Zhan <zhan_nicholas@outlook.com>
2020-12-13 15:18:23 +0800

    Add: deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+echo "Deploy started ..."
+echo "Deleting public directory ..."
+rd public
+echo "Building Sites ..."
+hugo
+echo "Publishing contents ..."
+cd public 
+git add .
+git commit -m "Rebuilding site"
+git push origin main
+echo "Finished"
